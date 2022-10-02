c4eda605859d9f0456c9e5f4bc45f3232925aaf3
ElemaGp <elemagodspower@gmail.com>
2022-10-01 18:15:17 -0700

    added Testimonial and Porfolio to the navbar

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -46,7 +46,7 @@ const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Review from clients</h5>
-      <h1>Testimonials</h1>
+      <h2>Testimonials</h2>
 
       <Swiper className="container testimonials__container" 
       // install Swiper modules
